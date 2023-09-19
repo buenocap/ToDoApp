427b64b5dc34f32d849dad07c376aa1c21754184
Pedro Bueno <pbueno159@gmail.com>
2023-09-19 16:05:00 -0600

    working on changing active link

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
+let tList = [];
+
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{
+        todayList: tList
+    });
+})
+
+app.post("/",(req,res)=>{
+    tList.push(req.body);
+    console.log(tList);
+    res.render("index.ejs",{
+        todayList: tList
+    });
+})
+
+app.get("/week",(req,res)=>{
+    res.render("week.ejs");
+})
+
+app.get("/month",(req,res)=>{
+    res.render("month.ejs");
 })
 
 app.listen(PORT,()=>{
